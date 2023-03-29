9d5787ff21395dacba417a49327919bc59044d13
Roberto Fernández Barrios <roberto.fernandez@opendeusto.es>
2023-03-29 13:33:02 +0200

    Update

--- Altera/Sesion3/04_Enunciado06.vhd
+++ Altera/Sesion3/04_Enunciado06.vhd
@@ -21,165 +21,213 @@ end main;
 architecture Behavioral of main is
 
 -- Editable
-
+signal flt: integer range 0 to 500000000;
+signal sal: std_logic;
+signal bt: std_logic;
+signal inicio: std_logic;
 signal estado: std_logic_vector (2 downto 0);
-signal cont_filtro: integer range 0 to 100000000;
-signal puls_sal: std_logic;
-signal contador_unidades_seg: std_logic_vector (3 downto 0);
 
-signal horas_decenas_alarma: std_logic_vector (3 downto 0);
-signal horas_unidades_alarma: std_logic_vector (3 downto 0);
-signal minutos_decenas_alarma: std_logic_vector (3 downto 0);
-signal minutos_unidades_alarma: std_logic_vector (3 downto 0);
-signal dato: std_logic_vector (3 downto 0);
+signal segundos: std_logic_vector (3 downto 0);
+signal segundos_decenas: std_logic_vector (3 downto 0);
+signal minutos: std_logic_vector (3 downto 0);
+signal minutos_decenas: std_logic_vector (3 downto 0);
+signal horas: std_logic_vector (3 downto 0);
+signal horas_decenas: std_logic_vector (1 downto 0);
 
-signal enable_seg_aux: std_logic_vector (3 downto 0);
-signal contador_base_enable: integer range 0 to 100000;
 
 begin
 
-process(clk, inicio)
-begin
-if inicio='1' then	
-	estado<="000";
-	cont_filtro<=0;
-elsif rising_edge(clk) then
-	case estado is
-    when "000" =>   cont_filtro<=0;
-                    if pulsador='1' then
-                        estado<="001";
-                    end if;                        
-    when "001" =>   cont_filtro<=cont_filtro+1;
-                    if pulsador='0' then
-                        estado<="000";
-                    elsif pulsador='1' and cont_filtro=100000 then
-                        estado<="010";
-                    end if;                        
-    when "010" =>   cont_filtro<=cont_filtro+1;
-                    if pulsador='0' then
-                        estado<="011";
-                    elsif cont_filtro=100000000 then 
-                        estado<="100";   
-                    end if;                        
-    when "011" =>   cont_filtro<=0;
-                    estado<="000";
-    when "100" =>   cont_filtro<=0;
-                    if pulsador='1' then
-                        estado<="101";
-                    else
-                        estado<="011";
-                    end if;        
-    when "101" =>   cont_filtro<=cont_filtro+1;
-                    if pulsador='0' then
-                        estado<="011";
-                    elsif cont_filtro=25000000 then
-                        estado<="100";
-                    end if;    
-    when others =>   cont_filtro<=0;
-                    estado<="000";
-	end case;
-end if;
-end process;
+inicio<=v_bt(0);
+bt<=v_bt(1);
+g_led(2 downto 0)<= estado;
 
-process(estado)
+process(inicio, bt, g_clock_50) -- Automata para poner la alarma
 begin
-case estado is
-	when "000" => puls_sal<='0';
-	when "001" => puls_sal<='0';
-	when "010" => puls_sal<='0';
-	when "011" => puls_sal<='1';
-	when "100" => puls_sal<='1';
-	when "101" => puls_sal<='0';
-	when others => puls_sal<='0';
-end case;
-end process;
-
-process(inicio, clk)
-begin
-if inicio='1' then
-    minutos_unidades_alarma<="0000";
-    minutos_decenas_alarma<="0000";
-    horas_unidades_alarma<="0000";
-    horas_decenas_alarma<="0000";
-elsif rising_edge(clk) then
-    if puls_sal='1' then
-    if minutos_unidades_alarma=9 then
-        minutos_unidades_alarma<="0000";
-        if minutos_decenas_alarma=5 then
-            minutos_decenas_alarma<="0000";
-            if horas_unidades_alarma=9 or (horas_unidades_alarma=3 and horas_decenas_alarma=2) then
-                horas_unidades_alarma<="0000";
-                if horas_unidades_alarma=3 and horas_decenas_alarma=2 then
-                    horas_decenas_alarma<="0000";
-                else
-                    horas_decenas_alarma<=horas_decenas_alarma+1;
-                end if;    
-            else
-                horas_unidades_alarma<=horas_unidades_alarma+1;
-            end if;    
-        else
-            minutos_decenas_alarma<=minutos_decenas_alarma+1;
-        end if;    
-    else
-        minutos_unidades_alarma<=minutos_unidades_alarma+1;
-    end if;    
+    if (inicio = '0') then
+        estado <= "000";
+        flt <= 0;
+        sal <= '0';
+    elsif ( rising_edge(g_clock_50) ) then
+        case estado is
+            when "000" =>
+                flt <= 0;
+                sal <= '0';
+                if (bt = '1') then
+                    estado <= "001";
+                elsif (bt = '0') then
+                    estado <= "000";
+                end if;
+            when "001" =>
+                flt <= flt + 1;
+                sal <= '0';
+                if ( (bt = '1') and (flt < 50000) ) then
+                    estado <= "001";
+                elsif ( (bt = '1') and (flt = 50000) ) then
+                    estado <= "010";
+                elsif (bt = '0') then
+                    estado <= "000";
+                end if;
+            when "010" =>
+                flt <= flt + 1;
+                sal <= '0';
+                if ( (bt = '1') and (flt < 50000000) ) then
+                    estado <= "001";
+                elsif ( (bt = '1') and (flt = 50000000) ) then
+                    estado <= "010";
+                elsif (bt = '0') then
+                    estado <= "011";
+                end if;
+            when "011" =>
+                flt <= 0;
+                sal <= '1';
+                estado <= "000";
+            when "100" =>
+                flt <= 0;
+                sal <= '1';
+                if (bt = '1') then
+                    estado <= "101";
+                elsif (bt = '0') then
+                    estado <= "011";
+                end if;
+            when "101" =>
+                flt <= flt + 1;
+                sal <= '0';
+                if ( (bt = '1') and (flt < (50000000 / 4) ) ) then
+                    estado <= "101";
+                elsif ( (bt = '1') and (flt = (50000000 / 4) ) ) then
+                    estado <= "100";
+                elsif (bt = '0') then
+                    estado <= "011";
+                end if;
+            when others =>
+                estado <= "000";
+                flt <= 0;
+                sal <= '0';
+        end case;
     end if;
-end if;
 end process;
 
-process(clk, inicio)
+process(inicio, g_clock_50, sal)
 begin
-if inicio='1' then
-	contador_base_enable<=0;
-elsif rising_edge(clk) then	
-	if contador_base_enable=100000 then
-		contador_base_enable<=0;
-	else
-		contador_base_enable<=contador_base_enable+1;
-	end if;
-end if;
+    if (inicio = '0') then
+        segundos <= "0000";
+        segundos_decenas <= "0000";
+        minutos  <= "0000";
+        minutos_decenas  <= "0000";
+        horas <= "0000";
+        horas_decenas <= "00";
+    elsif (sal = '1') then
+        if ( segundos = "1001" ) then 
+            segundos <= "0000";
+            if (segundos_decenas = "0101" ) then
+                segundos_decenas <= "0000";
+                if (minutos = "1001") then
+                    minutos <= "0000";
+                    if (minutos_decenas = "0101" ) then
+                        minutos_decenas <= "0000";
+                        if ((horas_decenas = "10") and (horas = "0011"))  then
+                            horas <= "0000";
+                            horas_decenas <= "00";
+                        elsif ( horas = "1001")  then
+                            horas <= "0000";
+                            horas_decenas <= horas_decenas + 1;
+                        else
+                            horas <= horas + 1;
+                        end if;
+                    else
+                        minutos_decenas <= minutos_decenas + 1;
+                    end if;
+                else
+                    minutos <= minutos + 1;
+                end  if;
+            else
+                segundos_decenas <= segundos_decenas + 1;
+            end if;
+        else 
+            segundos <= segundos + 1;
+        end if;
+    end if;
 end process;
 
-process(clk, inicio)
+process(segundos,segundos_decenas,minutos, minutos_decenas, horas, horas_decenas)
 begin
-if inicio='1' then
-	enable_seg_aux<="0111";
-elsif rising_edge(clk) then
-	if contador_base_enable=100000 then
-		enable_seg_aux<=enable_seg_aux(2 downto 0)&enable_seg_aux(3);
-	end if;
-end if;
+
+   case segundos is
+      when "0000" => g_hex0 <="0000001";
+      when "0001" => g_hex0 <="1001111";
+      when "0010" => g_hex0 <="0010010";
+      when "0011" => g_hex0 <="0000110";
+      when "0100" => g_hex0 <="1001100";
+      when "0101" => g_hex0 <="0100100";
+      when "0110" => g_hex0 <="1100000";
+      when "0111" => g_hex0 <="0001111";
+      when "1000" => g_hex0 <="0000000";
+      when "1001" => g_hex0 <="0001100";
+      when others => g_hex0 <="1111111";
+   end case;
+
+   case segundos_decenas is
+      when "0000" => g_hex1 <="0000001";
+      when "0001" => g_hex1 <="1001111";
+      when "0010" => g_hex1 <="0010010";
+      when "0011" => g_hex1 <="0000110";
+      when "0100" => g_hex1 <="1001100";
+      when "0101" => g_hex1 <="0100100";
+      when "0110" => g_hex1 <="1100000";
+      when "0111" => g_hex1 <="0001111";
+      when "1000" => g_hex1 <="0000000";
+      when "1001" => g_hex1 <="0001100";
+      when others => g_hex1 <="1111111";
+   end case;
+
+   case minutos is
+      when "0000" => g_hex2 <="0000001";
+      when "0001" => g_hex2 <="1001111";
+      when "0010" => g_hex2 <="0010010";
+      when "0011" => g_hex2 <="0000110";
+      when "0100" => g_hex2 <="1001100";
+      when "0101" => g_hex2 <="0100100";
+      when "0110" => g_hex2 <="1100000";
+      when "0111" => g_hex2 <="0001111";
+      when "1000" => g_hex2 <="0000000";
+      when "1001" => g_hex2 <="0001100";
+      when others => g_hex2 <="1111111";
+   end case;
+
+   case minutos_decenas is
+      when "0000" => g_hex3 <="0000001";
+      when "0001" => g_hex3 <="1001111";
+      when "0010" => g_hex3 <="0010010";
+      when "0011" => g_hex3 <="0000110";
+      when "0100" => g_hex3 <="1001100";
+      when "0101" => g_hex3 <="0100100";
+      when "0110" => g_hex3 <="1100000";
+      when "0111" => g_hex3 <="0001111";
+      when "1000" => g_hex3 <="0000000";
+      when "1001" => g_hex3 <="0001100";
+      when others => g_hex3 <="1111111";
+   end case;
+
+   case horas is
+      when "0000" => g_hex4 <="0000001";
+      when "0001" => g_hex4 <="1001111";
+      when "0010" => g_hex4 <="0010010";
+      when "0011" => g_hex4 <="0000110";
+      when "0100" => g_hex4 <="1001100";
+      when "0101" => g_hex4 <="0100100";
+      when "0110" => g_hex4 <="1100000";
+      when "0111" => g_hex4 <="0001111";
+      when "1000" => g_hex4 <="0000000";
+      when "1001" => g_hex4 <="0001100";
+      when others => g_hex4 <="1111111";
+   end case;
+
+   case horas_decenas is
+      when "00" => g_hex5 <="0000001";
+      when "01" => g_hex5 <="1001111";
+      when others => g_hex5 <="0010010";
+   end case;
+
 end process;
-
-process(enable_seg_aux, horas_decenas_alarma, horas_unidades_alarma, minutos_decenas_alarma, minutos_unidades_alarma)
-begin
-case enable_seg_aux is
-	when "0111" => dato<=horas_decenas_alarma;
-	when "1011" => dato<=horas_unidades_alarma;
-	when "1101" => dato<=minutos_decenas_alarma;
-	when "1110" => dato<=minutos_unidades_alarma;
-	when others => dato<="1111";
-end case;
-end process;
-
-process(dato)
-begin
-case dato is
-when "0000" => seven_seg<="0000001";
-when "0001" => seven_seg<="1001111";
-when "0010" => seven_seg<="0010010";
-when "0011" => seven_seg<="0000110";
-when "0100" => seven_seg<="1001100";
-when "0101" => seven_seg<="0100100";
-when "0110" => seven_seg<="1100000";
-when "0111" => seven_seg<="0001111";
-when "1000" => seven_seg<="0000000";
-when "1001" => seven_seg<="0001100";
-when others => seven_seg<="1111111";
-end case;
-end process;
-
-enable_seg<=enable_seg_aux;
-
 
 end Behavioral;
